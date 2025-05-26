351296f21c634436c0b37fd15c66fa3c9a9ecb8d
seba <sebaburgat@gmail.com>
2025-05-26 19:02:19 -0300

    se agrega manjo de sesion  con local storage para  navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,28 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import SwalReact from "sweetalert2-react-content";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const isAuth = localStorage.getItem("auth") === "true";
+  const username = localStorage.getItem("username");
+  const firstRender = localStorage.getItem("firstRender");
+
+  if (firstRender !== "true") {
+    localStorage.setItem("firstRender", "true");
+    SwalReact(Swal).fire({
+      title: "Informacion Importante",
+      text: "Usuario y contraseña para  el login son: juan y 123456. La ruta '/error' muestra manejo de error de api", 
+      icon: "info",
+      confirmButtonText: "Ok",
+    });
+  }
+  const cerrarSesion = () => {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
@@ -21,10 +41,36 @@ const NavBar = () => {
             <Nav.Link as={Link} to="/about">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
           </Nav>
+          <Nav>
+            {!isAuth ? (
+              <Nav.Link as={Link} to="/login">
+                <Button variant="outline-light">Login</Button>
+              </Nav.Link>
+            ) : (
+              <>
+                <Nav.Link className="mt-2">
+                  <span>Hola {username} !</span>
+                </Nav.Link>
+                <Nav.Link className="position-relative d-inline-flex align-items-center ">
+                  <i className="bi bi-cart-fill fs-4 "></i>
+                  <span className="position-absolute top-10 start-100 translate-middle badge rounded-pill bg-secondary">
+                    3
+                  </span>
+                </Nav.Link>
+                <Nav.Link className="ml-4">
+                  <Button
+                    className="ms-2 me-2"
+                    variant="outline-light"
+                    onClick={cerrarSesion}
+                  >
+                    Cerrar sesión
+                  </Button>
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Nav></Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
